723dbdc47593a60f88875497cabc085ca2935e04
SosthenesMS <sosthenesms@yahoo.com.br>
2023-03-24 08:50:17 -0300

    Criação do repository StatusVistoriaRepository.java

---
+++ vistoria-app/vistoria-app/src/main/java/com/vistoria/app/vistoriaapp/repository/StatusVistoriaRepository.java
@@ -0,0 +1,8 @@
+package com.vistoria.app.vistoriaapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.vistoria.app.vistoriaapp.model.StatusVistoria;
+
+public interface StatusVistoriaRepository extends JpaRepository<StatusVistoria, Integer> {
+
+}
