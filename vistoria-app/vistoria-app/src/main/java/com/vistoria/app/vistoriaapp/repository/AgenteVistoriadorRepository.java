efe957bb30f979ca9fd11cbd66a5b7fdc8bfbf4e
SosthenesMS <sosthenesms@yahoo.com.br>
2023-03-24 08:49:44 -0300

    Criação do repository AgenteVistoriadorRepository.java

---
+++ vistoria-app/vistoria-app/src/main/java/com/vistoria/app/vistoriaapp/repository/AgenteVistoriadorRepository.java
@@ -0,0 +1,8 @@
+package com.vistoria.app.vistoriaapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.vistoria.app.vistoriaapp.model.AgenteVistoriador;
+
+public interface AgenteVistoriadorRepository extends JpaRepository<AgenteVistoriador, Integer> {
+
+}
